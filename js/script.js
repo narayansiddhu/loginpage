44a991c5f5f44ffaeb2ac98632ca4dab4ce54c1e
narayan siddhu <narayansiddhu@gmail.com>
2018-03-23 22:11:40 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var button = document.getElementById('open').addEventListener('click', function(){
+    document.getElementById('main').style.display='block';
+  
+})
+document.getElementById('close').addEventListener('click',function(){
+document.getElementById('main').style.display='none';
+});
+
+function ValidationEvent() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var contact = document.getElementById("contact").value;
+    if (name != '' && email != '' && contact != '') {
+    alert("You have successfully registered");
+    return true;
+    } else {
+    alert("Please fill all details.....!");
+    return false;
+    }
+    }
